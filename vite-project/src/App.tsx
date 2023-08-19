4031be34ddde85fc76200fa4db67ea0a44825344
Devesh Krishan <deveshkrishan2003@gmail.com>
2023-08-19 18:07:24 +0530

    added tailwind css and started login page

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='flex gap-2 p-20 max-w-sw bg-white rounded-xl flex-col'>
+        <h1 className="text-2xl text-black font-bold text-center">
+          Login
+        </h1>
+        <label htmlFor="name" className='text-black'>Name:</label>
+        <input type="text" id="name" name="name" className='bg-gray-200 text-black'/>
+
+        <label htmlFor="email" className='text-black'>Email:</label>
+        <input type="email" id="email" name="email" className='bg-gray-200 text-black'/>
+
+        <label htmlFor="password" className='text-black'>Password:</label>
+        <input type="email" id="email" name="email" className='bg-gray-200 text-black'/>
+
+        <button className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4'>Submit</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
