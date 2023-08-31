739f75a38d6c2c1df333879bfcbe811806a11c63
Devesh Krishan <deveshkrishan2003@gmail.com>
2023-08-31 10:55:43 +0530

    Update queries.sql

--- backend/queries.sql
+++ backend/queries.sql
@@ -1,3 +1,6 @@
 -- Grab all info about a movie
 SELECT * FROM Movies
 
+-- Grabbing list of friends and date friendship given a username
+-- Givne username is User1
+SELECT User2Username, FriendshipStarted FROM Friends WHERE User1Username = Placeholder 
