84d6888f72f2c08b53984b0a982945c3ab79d720
austinhourscht <ahourscht31@gmail.com>
2023-08-31 02:09:46 -0700

    Queries 1-3 draft

--- backend/queries.sql
+++ backend/queries.sql
@@ -1,6 +1,37 @@
 -- Grab all info about a movie
 SELECT * FROM Movies
 
--- Grabbing list of friends and date friendship given a username
--- Givne username is User1
-SELECT User2Username, FriendshipStarted FROM Friends WHERE User1Username = Placeholder 
+--write a query to get list of friend names for a user and the date they became friends given a username--
+SELECT User2Username
+		,FriendshipStarted
+FROM Friends 
+WHERE User1Username = 'User1'
+
+--write a query to get user's favorite movies given a username--
+SELECT m.MovieName
+FROM Movies m
+	INNER JOIN
+		Favorites f
+		ON m.MovieId = f.MovieId
+		WHERE f.Username = 'User1'
+GROUP BY
+	m.MovieName
+--write a query to get all the reviews a user did along with its information given a username--
+SELECT r.Username
+			,m.MovieName
+			,m.MovieYear
+			,r.Score
+			,r.ReviewDate
+FROM Reviews r
+	INNER JOIN
+		Movies m
+		ON r.MovieId = m.MovieId
+	INNER JOIN
+		[User] u
+		ON r.Username = u.Username
+WHERE u.Username = 'User1'
+GROUP BY m.MovieName
+			,m.MovieYear
+			,r.Score
+			,r.ReviewDate
+-- write a query to get the average rating for a movie given a movie id --
