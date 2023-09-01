a80ced45da9b59995b7c2519b88f2ff0213b79d3
austinhourscht <ahourscht31@gmail.com>
2023-08-31 21:09:29 -0700

    Queries Complete
    with Nelson

--- backend/queries.sql
+++ backend/queries.sql
@@ -1,18 +1,22 @@
 --write a query to get list of friend names for a user and the date they became friends given a username--
+-- WORKS --
 SELECT User2Username
 		,FriendshipStarted
 FROM Friends 
-WHERE User1Username = 'User1'
+WHERE User1Username = 'testUser'
 
 --write a query to get user's favorite movies given a username--
-SELECT m.MovieName
+-- WORKS --
+SELECT f.Username
+		,m.MovieName
 FROM Movies m
 	INNER JOIN
 		Favorites f
 		ON m.MovieId = f.MovieId
-		WHERE f.Username = 'User1'
+		WHERE f.Username = 'testUser'
 GROUP BY
 	m.MovieName
+	,f.Username
 --write a query to get all the reviews a user did along with its information given a username--
 -- WORKS --
 SELECT r.Username
@@ -27,41 +31,49 @@ FROM Reviews r
 	INNER JOIN
 		[User] u
 		ON r.Username = u.Username
-WHERE u.Username = 'User1'
+WHERE u.Username = 'testUser'
 GROUP BY m.MovieName
 			,m.MovieYear
 			,r.Score
 			,r.ReviewDate
 			,r.Username
 -- write a query to get the average rating for a movie given a movie id --
+-- WORKS --
 SELECT m.MovieName
-		,AVG(r.Score)
+		,AVG(r.Score) AS 'Average Score'
 FROM Reviews r
 	INNER JOIN
 		Movies m
 		ON m.MovieId = r.MovieId
-		WHERE m.MovieID = 'MovieID'
-GROUP BY r.Score
+		WHERE m.MovieID = 1
+		GROUP BY m.MovieName
 --write a query to get the average rating a user gives in their reviews given a username--
+-- WORKS --
 SELECT u.Username
-		,AVG(r.Score)
+		,AVG(r.Score) AS 'Average Score'
 FROM Reviews r
 INNER JOIN
 	[User] u
 	ON r.Username = u.Username
-	WHERE u.Username = 'Username'
+	WHERE u.Username = 'testUser'
 	GROUP BY u.Username
 --write a query to get a movies information given a movieid--
-SELECT m.MovieName
+-- WORKS --
+SELECT m.MovieId
+		,m.MovieName
 		,m.MovieYear
 		,mg.Genre
 FROM Movies m
 INNER JOIN
 	MovieGenre mg
 	ON m.MovieId = mg.MovieGenreId
-	WHERE m.MovieId = 'MovieID'
-	GROUP BY m.MovieName
+	WHERE m.MovieId = 1
+	GROUP BY m.MovieId
+			,m.MovieName
+			,m.MovieYear
+			,mg.Genre
 --write a query to check if given a username, is the username already in the user table given a username--
+-- WORKS --
 SELECT u.Username
 FROM [User] u
-WHERE u.Username = 'Username'
+WHERE u.Username = 'testUser'
