69a6e08335000e9a9e1bb77dca0c2a8ac03d981c
austinhourscht <ahourscht31@gmail.com>
2023-08-31 20:39:04 -0700

    updated insert statements
    with Nelson

--- backend/insertStatements.sql
+++ backend/insertStatements.sql
@@ -1,2 +1,23 @@
+-- User --
+INSERT INTO [User](Username, [Password], Email, Active, Phone)
+VALUES('testUser','123456','testuser@gmail.com',0,'9163688200')
+
+-- Reviews --
 INSERT INTO Reviews(Username, ReviewDate, MovieId, Score)
 VALUES ('testUser', '8/31/2023', 1, 5.0)
+
+-- Movies --
+INSERT INTO Movies(MovieId, MovieName, MovieYear)
+VALUES(MovieID,'MovieName',MovieYear)
+
+-- MovieGenre --
+INSERT INTO MovieGenre(MovieId,Genre)
+VALUES('MovieID','MoveGenre')
+
+-- Friends --
+INSERT INTO Friends(User1Username,User2Username,FriendshipStarted)
+VALUES('User1Username','User2Username',2023-08-31)
+
+-- Favorites -- 
+INSERT INTO Favorites(Username, MovieId)
+VALUES('Username', 'MovieId')
